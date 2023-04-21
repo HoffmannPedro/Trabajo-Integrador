c3800bf78f7e8d396a6c40787e492fd0a553e6fa
Pedro Hoffmann <hoffmannpedro18@gmail.com>
2023-04-20 22:39:33 -0300

    Agregado reset de formulario luego de enviar el mismo

--- js/validate_form.js
+++ js/validate_form.js
@@ -1,56 +1,57 @@
-$(document).ready(function () {
-    function validarFormulario() {
-        let validadorFormulario = new FormValidator('registrarDatos', [{
-                name: 'nombre',
-                display: 'Nombre',
-                rules: 'required'
-            },
-            {
-                name: 'apellido',
-                display: 'Apellido',
-                rules: 'required'
-            },
-            {
-                name: 'telefono',
-                display: 'Teléfono',
-                rules: 'required|numeric'
-            },
-            {
-                name: 'email',
-                display: 'Email',
-                rules: 'required|valid_email'
-            }
-        ], function (errores, evento) {
-            if (errores.length) {
-                let mensaje = '';
-                let nombre = $('#nombre');
-                let apellido = $('#apellido');
-                let telefono = $('#telefono');
-                let email = $('#email');
-                let consulta = $('#mensaje');
+window.addEventListener('load', () => {
+    let validadorFormulario = new FormValidator('registrarDatos', [{
+            name: 'nombre',
+            display: 'Nombre',
+            rules: 'required'
+        },
+        {
+            name: 'apellido',
+            display: 'Apellido',
+            rules: 'required'
+        },
+        {
+            name: 'telefono',
+            display: 'Teléfono',
+            rules: 'required|numeric'
+        },
+        {
+            name: 'email',
+            display: 'Email',
+            rules: 'required|valid_email'
+        }
+    ], function (errores, evento) {
+        if (errores.length) {
+            var mensaje = '';
+            var nombre = $('#nombre');
+            var apellido = $('#apellido');
+            var telefono = $('#telefono');
+            var email = $('#email');
+            var consulta = $('#mensaje');
 
-                errores.forEach(function (campo, indice, arreglo) {
-                    mensaje += `${campo.message} <br/>`
-                });
+            errores.forEach(function (campo, indice, arreglo) {
+                mensaje += `${campo.message} <br/>`
+            });
 
-                document.querySelector('#aviso-error').innerHTML = mensaje;
-            } else {
-                evento.preventDefault();
-                document.querySelector('#aviso-error').innerHTML = "";
-                swal({
-                    title: '¡Muchas gracias!',
-                    text: 'Ya recibimos tu consulta, en la brevedad nos pondremos en contacto con vos',
-                    icon: 'success',
-                    button: 'Continuar',
-                    closeOnClickOutside: true,
-                    timer: 7000
-                });
-                let form = $('#registrarDatos')[0];
-                $(form).trigger("reset");
-            }
-        });
-        console.log('Agarrense de las manos');
-    }
+            document.querySelector('#aviso-error').innerHTML = mensaje;
+        } else {
+            evento.preventDefault();
+            document.querySelector('#aviso-error').innerHTML = "";
+            swal({
+                title: '¡Muchas gracias!',
+                text: 'Ya recibimos tu consulta, en la brevedad nos pondremos en contacto con vos',
+                icon: 'success',
+                button: 'Continuar',
+                closeOnClickOutside: true,
+                timer: 7000
+            });
+            let form = $('#registrarDatos')[0];
+            $(form).trigger("reset");
+            setTimeout(function () {
+                location.reload();
+            }, 3000);
+        }
+    })
+
     // Agregar evento de click al botón "Siguiente"
     $('.siguiente').on('click', function (event) {
         let nombre = $('#nombre').val();
